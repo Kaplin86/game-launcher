852491fdb8c4bcf89400d629ed730dfdcfa0eeba
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2025-11-01 01:28:49 -0400

    Simple functionality
    Time: 2 hour and 11 minutes

--- game-launcher/mainManager.gd
+++ game-launcher/mainManager.gd
@@ -1,5 +1,9 @@
 extends Node2D
 @export var ContainerNode : SidewaysUContainer = null
+var RunningAGame = false
+var CurrentlyRunning
+
+var TimeSinceInput = 0
 func _process(delta):
 	if !ContainerNode:
 		return
@@ -7,3 +11,10 @@ func _process(delta):
 		ContainerNode.changeToIndex(ContainerNode.getCurrentIndex() + 1)
 	if Input.is_action_just_pressed("ui_up"):
 		ContainerNode.changeToIndex(ContainerNode.getCurrentIndex() - 1)
+	if Input.is_action_just_pressed("ui_accept") and !RunningAGame:
+		var exe_path = ProjectSettings.globalize_path("user://index.exe")
+		CurrentlyRunning = OS.create_process(exe_path,["--fullscreen"])
+		RunningAGame = true
+
+func _ready():
+	$Point/AnimationPlayer.play("move")
