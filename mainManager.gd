e292cc2a212fbcf14850e34ff1638ea3c28c39c0
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2025-11-02 22:47:51 -0500

    Translation Toggle
    Time: 2 hour and 54 minutes
    
    Games launched will now have another command line tag to determine the locale. Though, a script must be added to the game to ensure that this actually sets the locale.

--- mainManager.gd
+++ mainManager.gd
@@ -12,18 +12,30 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		ContainerNode.changeToIndex(ContainerNode.getCurrentIndex() - 1)
 	if Input.is_action_just_pressed("ui_accept") and !RunningAGame:
-		var exe_path = ProjectSettings.globalize_path("user://index.exe")
-		CurrentlyRunning = OS.create_process(exe_path,["--fullscreen"])
-		RunningAGame = true
+		if ContainerNode.FocusedNode.get_meta("type","") == "play":
+			var exe_path = ProjectSettings.globalize_path("user://index.exe")
+			print(exe_path)
+			CurrentlyRunning = OS.create_process(exe_path,["--fullscreen","--locale=" + TranslationServer.get_locale()])
+			RunningAGame = true
+		elif  ContainerNode.FocusedNode.get_meta("type","") == "translate":
+			if TranslationServer.get_locale() == 'en':
+				TranslationServer.set_locale('ja')
+			else:
+				TranslationServer.set_locale('en')
 	
 	if RunningAGame:
 		if not OS.is_process_running(RunningAGame):
 			# Process exited
 			RunningAGame = false
 			print("Game crashed or closed unexpectedly.")
+	
+	if TranslationServer.get_locale() == 'en':
+		$Label.text = "choose a game"
+	else:
+		$Label.text = "ゲームを選ぶ"
 func _ready():
 	$Point/AnimationPlayer.play("move")
-
+	TranslationServer.set_locale('en')
 
 func _on_container_re_shuffled():
 	var colorToSet = ContainerNode.FocusedNode.get_meta("bgColor",Color("7e7ec4"))
